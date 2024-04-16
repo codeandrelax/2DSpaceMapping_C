e2aee589d22e7f706f645670425376ad916b6f7c
Anja <anja.djakovic@student.etf.unibl.org>
2024-04-16 23:26:38 +0200

    Issue #35 : Provjeravanje ispravnosti mikrokontrolera Dodan .c fajl kao testni primjerak za provjeru ispravnosti MCU

---
+++ led_blinking_test_01.c
@@ -0,0 +1,17 @@
+void main() {
+     
+    AD1PCFG = 0XFFFF;
+
+    // konfiguracija I/O portova
+    TRISA.F0 = 1;                 // RA0 input pin
+    TRISB = 0;                    // RB output pins
+
+    LATB = 0;                     // All RBn pins are set to zero
+
+    while(1){
+      if (PORTA.F0 == 1){        
+        LATB = ~LATB;
+        Delay(1000);       
+      }
+    }
+}
